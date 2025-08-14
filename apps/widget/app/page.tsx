f7be06a24af3864c272c1bbe7741ba0002d03832
pieroangulocubas <pieroangulocubas.17@gmail.com>
2025-08-14 23:20:34 +0200

    02: convex package

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -3,10 +3,7 @@ import { Button } from "@workspace/ui/components/button"
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World / Widget</h1>
-        <Button size="sm">Button</Button>
-      </div>
+      <h1>App Widget</h1>
     </div>
   )
 }
